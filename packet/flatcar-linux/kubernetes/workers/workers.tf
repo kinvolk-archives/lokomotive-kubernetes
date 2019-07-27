fb50a8873e92c48e36b360afb2f1eebe89ab1dc5
Johannes Liebermann <johannes@kinvolk.io>
2019-07-27 14:59:19 +0200

    Rename count to worker_count
    Variables can't be named 'count' since it's a reserved Terraform
    keyword.

--- packet/flatcar-linux/kubernetes/workers/workers.tf
+++ packet/flatcar-linux/kubernetes/workers/workers.tf
@@ -1,5 +1,5 @@
 resource "packet_device" "nodes" {
-  count            = "${var.count}"
+  count            = "${var.worker_count}"
   hostname         = "${var.cluster_name}-${var.pool_name}-worker-${count.index}"
   plan             = "${var.type}"
   facilities       = ["${var.facility}"]
@@ -36,7 +36,7 @@ data "template_file" "install" {
 }
 
 resource "packet_bgp_session" "bgp" {
-  count          = "${var.count}"
+  count          = "${var.worker_count}"
   device_id      = "${element(packet_device.nodes.*.id, count.index)}"
   address_family = "ipv4"
 }
