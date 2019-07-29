9bf3cccb4a068e7a92d53c6824cd896f71f119b6
Johannes Liebermann <johannes@kinvolk.io>
2019-07-29 15:35:39 +0200

    azure: Run `terraform 0.12upgrade`

--- azure/flatcar-linux/kubernetes/bootkube.tf
+++ azure/flatcar-linux/kubernetes/bootkube.tf
@@ -2,14 +2,15 @@
 module "bootkube" {
   source = "git::https://github.com/kinvolk/terraform-render-bootkube.git?ref=9c1ff5a9e8b3e98922980a097c49c8b3a903437a"
 
-  cluster_name          = "${var.cluster_name}"
-  api_servers           = ["${format("%s.%s", var.cluster_name, var.dns_zone)}"]
-  etcd_servers          = ["${formatlist("%s.%s", azurerm_dns_a_record.etcds.*.name, var.dns_zone)}"]
-  asset_dir             = "${var.asset_dir}"
+  cluster_name          = var.cluster_name
+  api_servers           = [format("%s.%s", var.cluster_name, var.dns_zone)]
+  etcd_servers          = formatlist("%s.%s", azurerm_dns_a_record.etcds.*.name, var.dns_zone)
+  asset_dir             = var.asset_dir
   networking            = "flannel"
-  pod_cidr              = "${var.pod_cidr}"
-  service_cidr          = "${var.service_cidr}"
-  cluster_domain_suffix = "${var.cluster_domain_suffix}"
-  enable_reporting      = "${var.enable_reporting}"
-  enable_aggregation    = "${var.enable_aggregation}"
+  pod_cidr              = var.pod_cidr
+  service_cidr          = var.service_cidr
+  cluster_domain_suffix = var.cluster_domain_suffix
+  enable_reporting      = var.enable_reporting
+  enable_aggregation    = var.enable_aggregation
 }
+
