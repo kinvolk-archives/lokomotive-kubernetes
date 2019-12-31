24e91ba2a0c8766150a7c26f8bdf80d221cfe7ab
Johannes Liebermann <johannes@kinvolk.io>
2019-12-31 16:01:25 +0100

    Remove unused variable dns_zone_id

--- packet/flatcar-linux/kubernetes/variables.tf
+++ packet/flatcar-linux/kubernetes/variables.tf
@@ -10,11 +10,6 @@ variable "dns_zone" {
   description = "AWS Route53 DNS Zone (e.g. aws.example.com)"
 }
 
-variable "dns_zone_id" {
-  type        = string
-  description = "AWS Route53 DNS Zone ID (e.g. Z3PAABBCFAKEC0)"
-}
-
 variable "project_id" {
   description = "Packet project ID (e.g. 405efe9c-cce9-4c71-87c1-949c290b27dc)"
 }
